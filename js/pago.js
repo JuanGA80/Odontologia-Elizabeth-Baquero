c50ac8d3a802233c9052578b8da5f691df964e79
leonelvalentinn <leonelvalentinn@gmail.com>
2023-07-18 15:17:53 -0600

    Edit

--- js/pago.js
+++ js/pago.js
@@ -1,17 +1,14 @@
 let listElements = document.querySelectorAll('.list__button--click');
 
 listElements.forEach( listElements =>{
-   listElements.addEventListener('click', ()=>{
-       listElements.classList.toggle('arrow');
-
-       let height = 0;
-       let menu = listElements.nextElementSibling;
-       if (menu.clientHeight == "0") {
-           height = menu.scrollHeight;
-       }
-       
-       menu.style.height = `${height}px`;
-   })
-
-
+  listElements.addEventListener('click', ()=>{
+      listElements.classList.toggle('arrow');
+      let height = 0;
+      let menu = listElements.nextElementSibling;
+      if (menu.clientHeight == "0") {
+          height = 150;
+      }
+      
+      menu.style.height = `${height}px`;
+  })
 });
