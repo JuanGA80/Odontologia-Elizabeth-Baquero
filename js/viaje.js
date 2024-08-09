444788771255b279f3178ce4cb868381d1b13d55
Vladbraham <abrahamdelacruzhedo@gmail.com>
2024-08-09 17:17:36 -0500

    Actuizacion paquetes

---
+++ js/viaje.js
@@ -0,0 +1,5 @@
+const info=document.getElementById("info");
+const btnViaje=document.getElementById("btnViaje1");
+btnViaje.addEventListener("click",()=>{ 
+    info.classList.toggle("aparecer")
+})
