6f6649a58f61722add27294144a11beac546c0ba
Vladbraham <abrahamdelacruzhedo@gmail.com>
2024-08-13 16:49:27 -0600

    nuevas secciones

--- js/viaje.js
+++ js/viaje.js
@@ -2,6 +2,10 @@ const modal = document.getElementById("modalV");
 const cerrar = document.getElementById("close1");
 const show = document.getElementById("More");
 
+const modal2 = document.getElementById("modalV2");
+const cerrar2 = document.getElementById("close2");
+const show2 = document.getElementById("More2");
+
 show.addEventListener("click", function()
     {
         modal.classList.add("openmodal");
@@ -12,6 +16,21 @@ show.addEventListener("click", function()
 cerrar.addEventListener("click", function()
     {
         modal.classList.remove("openmodal");
+       
+    }
+
+)
+
+show2.addEventListener("click", function()
+    {
+        modal2.classList.add("openmodal2");
+       
+    }
+
+)
+cerrar2.addEventListener("click", function()
+    {
+        modal2.classList.remove("openmodal2");
        
     }
 
